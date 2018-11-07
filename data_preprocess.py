a6931ceda18f3f16b9e159f8a07450d07e4ea24f
snolanb <snolanb@gmail.com>
2018-11-07 15:59:49 -0600

    initial

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 DATA_DIR = './historical_hourly_weather_data/'
 temperature = pd.read_csv(DATA_DIR+'temperature.csv')
-temperature.applymap(lambda k: k * 9.0/5.0 - 459.67)
+cities = list(temperature)
+cities.pop(0)
+temperature[cities] = temperature[cities].applymap(lambda k: k * 9.0/5.0 - 459.67)
 print(temperature.dtypes)
 temperature.head()
